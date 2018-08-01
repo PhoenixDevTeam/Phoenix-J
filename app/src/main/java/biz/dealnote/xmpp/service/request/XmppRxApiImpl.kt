88578aa850aed7a0617fb02d2c652cbd830a6a38
dealnote.dev <dealnote.dev@gmail.com>
2018-08-01 17:40:02 +0300

    kotlin WIP

--- app/src/main/java/biz/dealnote/xmpp/service/request/XmppRxApiImpl.kt
+++ app/src/main/java/biz/dealnote/xmpp/service/request/XmppRxApiImpl.kt
@@ -2,10 +2,13 @@ package biz.dealnote.xmpp.service.request
 
 import biz.dealnote.xmpp.service.IConnectionManager
 import biz.dealnote.xmpp.util.safelyWait
+import io.reactivex.Completable
 import io.reactivex.Single
+import org.jivesoftware.smack.AbstractXMPPConnection
 import org.jivesoftware.smackx.vcardtemp.VCardManager
 import org.jivesoftware.smackx.vcardtemp.packet.VCard
 import org.jxmpp.jid.impl.JidCreate
+import java.util.concurrent.Callable
 import java.util.concurrent.CountDownLatch
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
@@ -13,45 +16,79 @@ import java.util.concurrent.Executors
 class XmppRxApiImpl(private val connectionManager: IConnectionManager) : IXmppRxApi {
 
     override fun getVCard(acccount: Int, jid: String): Single<VCard> {
-        return Single.create { emitter ->
-            val connection = connectionManager.findConnectionFor(acccount)
+        return singleFromCallable(xmppExecutor, Callable {
+            val connection: AbstractXMPPConnection = connectionManager.findConnectionFor(acccount)!!
+            val entityBareJid = JidCreate.entityBareFrom(jid)
+            return@Callable VCardManager.getInstanceFor(connection).loadVCard(entityBareJid)
+        })
+    }
 
-            connection?.run {
-                val vcardManager = VCardManager.getInstanceFor(connection)
-                val entityBareJid = JidCreate.entityBareFrom(jid)
-
+    companion object {
+        fun comletableFromRunnable(executorService: ExecutorService, r: Runnable): Completable {
+            return Completable.create { emitter ->
                 val latch = CountDownLatch(1)
                 emitter.setCancellable { latch.countDown() }
 
-                var result: Result<VCard> = Result()
+                var error: Throwable? = null
+                var complete = false
+
                 val runnable = Runnable {
-                    result = try {
-                        Result(data = vcardManager.loadVCard(entityBareJid))
+                    try {
+                        r.run()
+                        complete = true
                     } catch (e: Exception) {
-                        Result(error = e)
+                        error = e
                     }
 
                     latch.countDown()
                 }
 
-                xmppExecutor.submit(runnable)
+                executorService.submit(runnable)
 
-                if(latch.safelyWait() && !emitter.isDisposed){
-                    result.data?.run {
+                if (latch.safelyWait() && !emitter.isDisposed) {
+                    error?.run {
+                        emitter.onError(this)
+                    }
+
+                    if (complete) {
+                        emitter.onComplete()
+                    }
+                }
+            }
+        }
+
+        fun <T : Any> singleFromCallable(executorService: ExecutorService, callable: Callable<T>): Single<T> {
+            return Single.create { emitter ->
+                val latch = CountDownLatch(1)
+                emitter.setCancellable { latch.countDown() }
+
+                var result: T? = null
+                var error: Throwable? = null
+
+                val runnable = Runnable {
+                    try {
+                        result = callable.call()
+                    } catch (e: Exception) {
+                        error = e
+                    }
+
+                    latch.countDown()
+                }
+
+                executorService.submit(runnable)
+
+                if (latch.safelyWait() && !emitter.isDisposed) {
+                    result?.run {
                         emitter.onSuccess(this)
                     }
 
-                    result.error?.run {
+                    error?.run {
                         emitter.onError(this)
                     }
                 }
             }
         }
-    }
 
-    class Result<T> (val data: T? = null, val error: Throwable? = null)
-
-    companion object {
         private val xmppExecutor: ExecutorService = Executors.newSingleThreadExecutor()
     }
 }
